f1e1a6aac618baf562b9bbb58b71d9f1f1a6dbe2
MaXiang <71162666+Toursslivers@users.noreply.github.com>
2023-04-15 00:11:53 +0800

    feat: add qr code auth

--- src/components/auth/AuthQrCode.tsx
+++ src/components/auth/AuthQrCode.tsx
@@ -1,146 +1,201 @@
-import { useAtom } from "jotai";
+import React, { useEffect, useRef, memo, useCallback, FC } from "react";
 import {
-  memo,
-  FormEvent,
-  useRef,
-  useState,
-  useEffect,
-  useCallback,
-} from "react";
-import {
-  authErrorAtom,
-  authIsCodeViaAppAtom,
-  authIsLoadingAtom,
-  authPhoneNumberAtom,
   authQrCodeAtom,
   authStateAtom,
   connectionStateAtom,
   languageAtom,
 } from "../../global";
-import { IS_TOUCH_ENV } from "../../util/windowEnvironment";
+import { DEFAULT_LANG_CODE } from "../../config";
+import { LOCAL_TGS_URLS } from "../common/helpers/animatedAssets";
+import { setLanguage } from "../../util/langProvider";
+import clsx from "clsx";
 import renderText from "../common/helpers/renderText";
-import useLang from "../../hooks/useLang";
 import { getSuggestedLanguage } from "./helpers/getSuggestedLanguage";
-import useHistoryBack from "../../hooks/useHistoryBack";
-import {
-  clearAuthError,
-  returnToAuthPhoneNumber,
-  setAuthCode,
-} from "../../global/actions";
-// import TrackingMonkey from "../common/TrackingMonkey";
-import InputText from "../ui/InputText";
+
+import useLangString from "../../hooks/useLangString";
+import useFlag from "../../hooks/useFlag";
+import useLang from "../../hooks/useLang";
+import useMediaTransition from "../../hooks/useMediaTransition";
+import useAsync from "../../hooks/useAsync";
+
 import Loading from "../ui/Loading";
+import Button from "../ui/Button";
+import AnimatedIcon from "../common/AnimatedIcon";
 
-const CODE_LENGTH = 5;
+import blankUrl from "../../assets/blank.png";
+import { useAtom } from "jotai";
+import {
+  returnToAuthPhoneNumber,
+  setSettingOption,
+} from "../../global/actions";
 
-const AuthQrCode = () => {
+const DATA_PREFIX = "tg://login?token=";
+const QR_SIZE = 280;
+const QR_PLANE_SIZE = 54;
+
+let qrCodeStylingPromise: Promise<typeof import("qr-code-styling")>;
+
+function ensureQrCodeStyling() {
+  if (!qrCodeStylingPromise) {
+    qrCodeStylingPromise = import("qr-code-styling");
+  }
+  return qrCodeStylingPromise;
+}
+
+const AuthQrCode: FC = () => {
   const [connectionState] = useAtom(connectionStateAtom);
   const [authState] = useAtom(authStateAtom);
   const [authQrCode] = useAtom(authQrCodeAtom);
   const [language] = useAtom(languageAtom);
-  const [authError] = useAtom(authErrorAtom);
-  const [authPhoneNumber] = useAtom(authPhoneNumberAtom);
-  const [authIsCodeViaApp] = useAtom(authIsCodeViaAppAtom);
-  const [authIsLoading] = useAtom(authIsLoadingAtom);
 
   const suggestedLanguage = getSuggestedLanguage();
   const lang = useLang();
+  const qrCodeRef = useRef<HTMLDivElement>(null);
+  const continueText = useLangString(
+    suggestedLanguage,
+    "ContinueOnThisLanguage",
+    true
+  );
+  const [isLoading, markIsLoading, unmarkIsLoading] = useFlag();
+  const [isQrMounted, markQrMounted, unmarkQrMounted] = useFlag();
 
-  const inputRef = useRef<HTMLInputElement>(null);
-
-  const [code, setCode] = useState<string>("");
-  const [isTracking, setIsTracking] = useState(false);
-  const [trackingDirection, setTrackingDirection] = useState(1);
-
-  useEffect(() => {
-    if (!IS_TOUCH_ENV) {
-      inputRef.current!.focus();
-    }
+  const { result: qrCode } = useAsync(async () => {
+    const QrCodeStyling = (await ensureQrCodeStyling()).default;
+    return new QrCodeStyling({
+      width: QR_SIZE,
+      height: QR_SIZE,
+      image: blankUrl,
+      margin: 10,
+      type: "svg",
+      dotsOptions: {
+        type: "rounded",
+      },
+      cornersSquareOptions: {
+        type: "extra-rounded",
+      },
+      imageOptions: {
+        imageSize: 0.4,
+        margin: 8,
+      },
+      qrOptions: {
+        errorCorrectionLevel: "M",
+      },
+    });
   }, []);
 
-  useHistoryBack({
-    isActive: true,
-    onBack: returnToAuthPhoneNumber,
-  });
+  const transitionClassNames = useMediaTransition(isQrMounted);
 
-  const onCodeChange = useCallback(
-    (e: FormEvent<HTMLInputElement>) => {
-      if (authError) {
-        clearAuthError();
-      }
+  useEffect(() => {
+    if (!authQrCode || !qrCode) {
+      return () => {
+        unmarkQrMounted();
+      };
+    }
 
-      const { currentTarget: target } = e;
-      target.value = target.value.replace(/[^\d]+/, "").substr(0, CODE_LENGTH);
+    if (connectionState !== "connectionStateReady") {
+      return undefined;
+    }
 
-      if (target.value === code) {
-        return;
-      }
+    const container = qrCodeRef.current!;
+    const data = `${DATA_PREFIX}${authQrCode.token}`;
 
-      setCode(target.value);
+    qrCode.update({
+      data,
+    });
 
-      if (!isTracking) {
-        setIsTracking(true);
-      } else if (!target.value.length) {
-        setIsTracking(false);
-      }
+    if (!isQrMounted) {
+      qrCode.append(container);
+      markQrMounted();
+    }
+    return undefined;
+  }, [
+    connectionState,
+    authQrCode,
+    isQrMounted,
+    markQrMounted,
+    unmarkQrMounted,
+    qrCode,
+  ]);
 
-      if (code && code.length > target.value.length) {
-        setTrackingDirection(-1);
-      } else {
-        setTrackingDirection(1);
-      }
+  useEffect(() => {
+    if (connectionState === "connectionStateReady") {
+      void setLanguage(DEFAULT_LANG_CODE);
+    }
+  }, [connectionState]);
 
-      if (target.value.length === CODE_LENGTH) {
-        setAuthCode({ code: target.value });
-      }
-    },
-    [authError, clearAuthError, code, isTracking, setAuthCode]
-  );
+  const handleLangChange = useCallback(() => {
+    markIsLoading();
 
-  function handleReturnToAuthPhoneNumber() {
+    void setLanguage(suggestedLanguage, () => {
+      unmarkIsLoading();
+
+      setSettingOption({ language: suggestedLanguage });
+    });
+  }, [markIsLoading, setSettingOption, suggestedLanguage, unmarkIsLoading]);
+
+  const habdleReturnToAuthPhoneNumber = useCallback(() => {
     returnToAuthPhoneNumber();
-  }
+  }, [returnToAuthPhoneNumber]);
+
+  const isAuthReady = authState === "authorizationStateWaitQrCode";
 
   return (
-    <div id="auth-code-form" className="custom-scroll">
-      <div className="auth-form">
-        {/* <TrackingMonkey
-          code={code}
-          codeLength={CODE_LENGTH}
-          isTracking={isTracking}
-          trackingDirection={trackingDirection}
-        /> */}
-        <h1>
-          {authPhoneNumber}
+    <div id="auth-qr-form" className="custom-scroll">
+      <div className="auth-form qr">
+        <div className="qr-outer">
           <div
-            className="auth-number-edit"
-            onClick={handleReturnToAuthPhoneNumber}
-            role="button"
-            tabIndex={0}
-            title={lang("WrongNumber")}
+            className={clsx("qr-inner", transitionClassNames)}
+            key="qr-inner"
           >
-            <i className="icon-edit" />
+            <div
+              key="qr-container"
+              className="qr-container"
+              ref={qrCodeRef}
+              style={`width: ${QR_SIZE}px; height: ${QR_SIZE}px`}
+            />
+            <AnimatedIcon
+              tgsUrl={LOCAL_TGS_URLS.QrPlane}
+              size={QR_PLANE_SIZE}
+              className="qr-plane"
+              nonInteractive
+              noLoop={false}
+            />
           </div>
-        </h1>
-        <p className="note">
-          {renderText(
-            lang(authIsCodeViaApp ? "SentAppCode" : "Login.JustSentSms"),
-            ["simple_markdown"]
+          {!isQrMounted && (
+            <div className="qr-loading">
+              <Loading />
+            </div>
           )}
-        </p>
-        <InputText
-          inputRef={inputRef}
-          id="sign-in-code"
-          label={lang("Code")}
-          onInput={onCodeChange}
-          value={code}
-          error={authError && lang(authError)}
-          autoComplete="off"
-          inputMode="numeric"
-        />
-        {authIsLoading && <Loading />}
+        </div>
+        <h1>{lang("Login.QR.Title")}</h1>
+        <ol>
+          <li>
+            <span>{lang("Login.QR.Help1")}</span>
+          </li>
+          <li>
+            <span>
+              {renderText(lang("Login.QR2.Help2"), ["simple_markdown"])}
+            </span>
+          </li>
+          <li>
+            <span>{lang("Login.QR.Help3")}</span>
+          </li>
+        </ol>
+        {isAuthReady && (
+          <Button isText onClick={habdleReturnToAuthPhoneNumber}>
+            {lang("Login.QR.Cancel")}
+          </Button>
+        )}
+        {suggestedLanguage &&
+          suggestedLanguage !== language &&
+          continueText && (
+            <Button isText isLoading={isLoading} onClick={handleLangChange}>
+              {continueText}
+            </Button>
+          )}
       </div>
     </div>
   );
 };
+
 export default memo(AuthQrCode);
---
+++ src/components/common/AnimatedIcon.tsx
@@ -0,0 +1,64 @@
+import React, { memo, useCallback, useState } from "react";
+
+import type { OwnProps as AnimatedStickerProps } from "./AnimatedSticker";
+
+import clsx from "clsx";
+import useMediaTransition from "../../hooks/useMediaTransition";
+import useFlag from "../../hooks/useFlag";
+
+import AnimatedSticker from "./AnimatedSticker";
+
+const DEFAULT_SIZE = 150;
+
+export type OwnProps = Partial<AnimatedStickerProps> & {
+  noTransition?: boolean;
+  nonInteractive?: boolean;
+};
+
+function AnimatedIcon(props: OwnProps) {
+  const {
+    size = DEFAULT_SIZE,
+    play = true,
+    noLoop = true,
+    className,
+    noTransition,
+    nonInteractive,
+    onLoad,
+    onClick,
+    ...otherProps
+  } = props;
+  const [isAnimationLoaded, markAnimationLoaded] = useFlag(false);
+  const transitionClassNames = useMediaTransition(
+    noTransition || isAnimationLoaded
+  );
+
+  const handleLoad = useCallback(() => {
+    markAnimationLoaded();
+    onLoad?.();
+  }, [markAnimationLoaded, onLoad]);
+
+  const [playKey, setPlayKey] = useState(String(Math.random()));
+
+  const handleClick = useCallback(() => {
+    if (play === true) {
+      setPlayKey(String(Math.random()));
+    }
+
+    onClick?.();
+  }, [onClick, play]);
+
+  return (
+    <AnimatedSticker
+      className={clsx(className, transitionClassNames)}
+      size={size}
+      play={play === true ? playKey : play}
+      noLoop={noLoop}
+      onClick={!nonInteractive ? handleClick : undefined}
+      onLoad={handleLoad}
+      /* eslint-disable-next-line react/jsx-props-no-spreading */
+      {...otherProps}
+    />
+  );
+}
+
+export default memo(AnimatedIcon);
